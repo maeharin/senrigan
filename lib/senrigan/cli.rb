67d9108bb0f4c6801d98493689a970b9ac920dba
maeharin <maeharin@gmail.com>
2014-08-11 07:58:00 +0900

    refactor cli

---
+++ lib/senrigan/cli.rb
@@ -0,0 +1,36 @@
+require 'senrigan'
+require 'thor'
+
+module Senrigan
+  class Cli < ::Thor
+    class_option :"display-edge", type: :boolean
+
+    desc "visualize jsp", "visualize jsp"
+    def jsp(jsp_path)
+      setup
+
+      # 一旦引数はディレクトリと想定
+      root_path= Pathname.new jsp_path
+
+      Dir.glob("#{root_path}/**/*.jsp").each do |file|
+        file_path = Pathname.new file
+        next if file_path.directory?
+
+        resource = Senrigan::Resource::Jsp.new(file_path, root_path)
+        resource.parse
+
+        Senrigan.register resource
+      end
+
+      Senrigan.generate
+    end
+
+    private
+
+    def setup
+      Senrigan.setup({
+        is_display_edge: options[:"display-edge"] ? true : false
+      })
+    end
+  end
+end
